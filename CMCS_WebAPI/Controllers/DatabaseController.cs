c03d634e77ed882538128521115b310f8384450c
Matthew De Waal <ST10069070@vcconnect.edu.za>
2024-11-21 15:19:35 +0200

    Project Update. Version: 2.4: Web API implemented.

---
+++ CMCS_WebAPI/Controllers/DatabaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMCS_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
